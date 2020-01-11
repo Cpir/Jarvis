04f9adcd15f4144c3017db15b09b3b7997bb9f04
Руслан <pycek@list.ru>
2020-01-11 22:02:15 +0300

    Добавьте файлы проекта.

---
+++ Jarvis/Speech.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Speech.Recognition;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Jarvis
+{
+    public class Speech
+    {
+        private GCHandle handle;
+        private CultureInfo cultureInfo;
+        private SpeechRecognitionEngine speechRecognitionEngine;
+
+        public Speech()
+        {
+            handle = GCHandle.Alloc(this);
+            cultureInfo = new CultureInfo("ru-RU");
+        }
+
+        public void Speak(string FilePath)
+        {
+
+
+
+        }
+
+
+
+        private void SpeechEngineConfigure()
+        {
+
+        }
+    }
+}
