ecae5d0a5f49602a83d903156edae7f191a14c6c
Руслан <pycek@list.ru>
2020-01-12 00:23:54 +0300

    123

--- Jarvis/Program.cs
+++ Jarvis/Program.cs
@@ -4,85 +4,17 @@ using System.Threading;
 using Microsoft.Speech.Recognition;
 using Microsoft.Speech.Synthesis;
 
-namespace Jarvis
+namespace PRMover
 {
     class Program
     {
-        static bool completed;
-
         static void Main(string[] args)
         {
-            CultureInfo culture = new CultureInfo("ru-RU");
-
-            SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine();
-
-            Grammar testGrammar =
-             new Grammar(new GrammarBuilder("ЕЕЕЕИИБААААТЬ КОПАТЬ")) { Name = "Test Grammar" };
-            recognizer.LoadGrammar(testGrammar);
-
-            recognizer.SpeechRecognized += Recognizer_SpeechRecognized;
-            recognizer.EmulateRecognizeCompleted += Recognizer_EmulateRecognizeCompleted;
-
-            recognizer.SetInputToWaveFile($"{AppDomain.CurrentDomain.BaseDirectory}1.wav");
-
-            recognizer.Recognize();
-            completed = false;
-
-            // Start asynchronous emulated recognition.   
-            // This matches the grammar and generates a SpeechRecognized event.  
-
-            // Wait for the asynchronous operation to complete.  
-
-
-
-            static void Recognizer_EmulateRecognizeCompleted(object sender, EmulateRecognizeCompletedEventArgs e)
-            {
-                throw new NotImplementedException();
-            }
-
-            static void Recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
-            {
-                throw new NotImplementedException();
-            }
-            while (true)
-            {
-                Thread.Sleep(0);
-            }
-            Console.WriteLine();
-            Console.WriteLine("Press any key to exit...");
-            Console.ReadKey();
-
-
-
-            // Handle the SpeechRecognized event.  
-            //static void SpeechRecognizedHandler(
-            //  object sender, SpeechRecognizedEventArgs e)
-            //{
-            //    if (e.Result != null)
-            //    {
-            //        Console.WriteLine("Recognition result = {0}",
-            //          e.Result.Text ?? "<no text>");
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("No recognition result");
-            //    }
-            //}
-
-            //// Handle the SpeechRecognizeCompleted event.  
-            //static void EmulateRecognizeCompletedHandler(
-            //  object sender, EmulateRecognizeCompletedEventArgs e)
-            //{
-            //    if (e.Result == null)
-            //    {
-            //        Console.WriteLine("No result generated.");
-            //    }
-
-            //    // Indicate the asynchronous operation is complete.  
-            //    completed = true;
-            //}
-
-
+            Jarvis jarvis = new Jarvis();
+            
+            jarvis.Speech.Speak("ЕЕЕЕЕББББАААААТЬ кооооопаааааааааааать");
+            
+            Console.ReadLine();
         }
     }
 }
--- Jarvis/Speech.cs
+++ Jarvis/Speech.cs
@@ -1,36 +1,159 @@
 ﻿using Microsoft.Speech.Recognition;
+using Microsoft.Speech.Synthesis;
 using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Runtime.InteropServices;
-using System.Text;
+using System.Threading;
 
-namespace Jarvis
+namespace PRMover
 {
-    public class Speech
+    public class Jarvis : IDisposable
     {
         private GCHandle handle;
-        private CultureInfo cultureInfo;
-        private SpeechRecognitionEngine speechRecognitionEngine;
+        private CultureInfo cultureInfo = new CultureInfo("ru-RU");
+        private Speech speech;
+
+        public Speech Speech
+        {
+            get { return speech; }
+            set { speech = value; }
+        }
+
+        public Jarvis()
+        {
+            handle = GCHandle.Alloc(this);
+            Speech = new Speech();
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+                if (disposing)
+                {
+                    handle.Free();
+                    Speech.Dispose();
+                }
+            disposedValue = true;
+        }
+
+        ~Jarvis() { Dispose(false); }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+
+    public class Speech : IDisposable
+    {
+        private GCHandle handle;
+        private CultureInfo cultureInfo = new CultureInfo("ru-RU");
+        private SpeechRecognitionEngine recognition;
+        private SpeechSynthesizer synthesizer;
+        private ReadOnlyCollection<InstalledVoice> voices;
+        private Thread mainWorker;
+        private Thread supportWorker;
+        private bool recognitionReady = false, synthesizerReady = false;
 
         public Speech()
         {
             handle = GCHandle.Alloc(this);
-            cultureInfo = new CultureInfo("ru-RU");
+            mainWorker = new Thread(() => RecognitionConfigure()) { Priority = ThreadPriority.Highest };
+            supportWorker = new Thread(() => SynthesizerConfigure()) { Priority = ThreadPriority.Highest };
+            mainWorker.Start(); mainWorker.Join();
+            supportWorker.Start(); supportWorker.Join();
         }
 
-        public void Speak(string FilePath)
+        public ReadOnlyCollection<InstalledVoice> Voices
+        {
+            get { return voices; }
+            set { voices = value; }
+        }
+
+        public CultureInfo CultureInfo
+        {
+            get { return cultureInfo; }
+            set { cultureInfo = value; }
+        }
+
+        public void Speak(string Text)
+        {
+            while (true)
+                synthesizer.SpeakAsync(Text);
+        }
+
+        private void RecognitionConfigure()
         {
 
-
-
         }
 
-
-
-        private void SpeechEngineConfigure()
+        private void SynthesizerConfigure()
         {
+            synthesizer = new SpeechSynthesizer();
+            synthesizer.SetOutputToDefaultAudioDevice();
+            Voices = synthesizer.GetInstalledVoices();
 
+            if (!synthesizerReady)
+            {
+                synthesizer.SpeakStarted +=
+                    new EventHandler<SpeakStartedEventArgs>(Synthesizer_SpeakStarted);
+                synthesizer.SpeakProgress +=
+                    new EventHandler<SpeakProgressEventArgs>(Synthesizer_SpeakProgress);
+                synthesizer.BookmarkReached +=
+                     new EventHandler<BookmarkReachedEventArgs>(Synthesizer_BookmarkReached);
+                synthesizer.SpeakCompleted +=
+                    new EventHandler<SpeakCompletedEventArgs>(Synthesizer_SpeakCompleted);
+            }
+
+            synthesizerReady = true;
         }
+
+        #region События синтезатора речи
+        private void Synthesizer_BookmarkReached(object sender, BookmarkReachedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Synthesizer_SpeakCompleted(object sender, SpeakCompletedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Synthesizer_SpeakProgress(object sender, SpeakProgressEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Synthesizer_SpeakStarted(object sender, SpeakStartedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region IDisposable Support
+        private bool disposedValue = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+                if (disposing)
+                    handle.Free();
+            disposedValue = true;
+        }
+
+        ~Speech() { Dispose(false); }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
